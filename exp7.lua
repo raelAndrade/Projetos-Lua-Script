bf0f405b83d76782573107cb1bbaf91811085375
israel <israelg.andrade@bol.com.br>
2019-01-08 13:32:49 -0200

    Iterando lista com regex

--- exp7.lua
+++ exp7.lua
@@ -1,10 +1,18 @@
 -- tbl = {"a", "c", "b", "d", "t"}
 -- url = "https://banco.bradesco/html/classic/produtos-servicos/cartoes/meu-cartao/bradesco-elo-nanquim/beneficios.shtm"
 
-a, b, c, d = string.match("https://banco.bradesco/html", '(%a+)://(%a+).(%a+)/+(%a+)(.-)')
+-- a, b, c, d = string.match("https://banco.bradesco/html", '(%a+)://(%a+).(%a+)/+(%a+)(.-)')
+local tbl = {string.match("https://banco.bradesco/html", '(%a+)://(%a+).(%a+)/+(%a+)(.-)')}
 
 --print(ini, c)
-print("Primeira ocorrencia: " .. a)
-print("Segunda ocorrencia: " .. b)
-print("Terceira ocorrencia: " .. c)
-print("Quarta ocorrencia: " .. d)
+--print("Primeira ocorrencia: " .. a)
+--print("Segunda ocorrencia: " .. b)
+--print("Terceira ocorrencia: " .. c)
+--print("Quarta ocorrencia: " .. d)
+
+while tbl ~= nil do
+	for i, f in ipairs(tbl) do
+		print(i, f)
+	end
+	break
+end
--- exp8.lua
+++ exp8.lua
@@ -1,13 +1,19 @@
 
 local frutas = { "laranja", "banana", "maça", "melancia", "morango", "pera" }
 
--- while frutas ~= nil do
--- 	print(frutas[1])
--- 	break
--- end
-
-for i, f in ipairs(frutas) do
-	if f ~= nil then
-		print(f)
+while frutas ~= nil do
+	for i, v in ipairs(frutas) do
+		print(frutas[i])
 	end
+	break
 end
+
+-- local lista = {}
+-- --for i, f in ipairs(frutas) do
+-- 	if frutas ~= nil then
+-- 		lista = frutas
+-- 		for i, f in ipairs(lista) do
+-- 			print(f)
+-- 		end
+-- 	end
+--end
